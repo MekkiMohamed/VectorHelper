c3546a7f9dba5fb92c64c5f9eceba9557e47bf7d
MekkiMohamed <fm_mekki@esi.dz>
2017-10-19 14:59:46 +0100

    l'ajout de la methode somme(vec1,vec2) et la classe VectorException()

---
+++ VectorException.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Bureau on 19/10/2017.
+ */
+public class VectorException extends Exception {
+    public String getMessage(){
+        return "lengths don't match";
+    }
+}
--- VectorHelper.java
+++ VectorHelper.java
@@ -2,5 +2,13 @@
  * Created by Bureau on 19/10/2017.
  */
 public class VectorHelper {
+    public int[]  somme(int[] vec1,int[] vec2)throws Exception{
+        if (vec1.length!=vec2.length)  throw new VectorException();
+        int[] tab=new int[vec1.length];
+        for(int i=0;i<tab.length;i++){
+            tab[i]=vec1[i]+vec2[i];
+        }
+        return tab;
+    }
 
 }
