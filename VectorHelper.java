b8680e79b92803238291af6b6a961fe5edb79e42
ritsurito21 <y.kardjadja@gmail.com>
2017-10-19 14:00:58 +0100

    just added the (invers) function

--- VectorHelper.java
+++ VectorHelper.java
@@ -1,9 +1,10 @@
 public class VectorHelper {
 
-    private int n;
 
-    void trie(int[] vecteur){
-        for(int i=0;i<vecteur.length;i++){
+
+    public void trie(int[] vecteur){
+        int n;
+        for(int i=0 ; i<vecteur.length ; i++){
             for(int j=i+1 ; j<vecteur.length ; i++){
                 if(vecteur[i]>vecteur[j]) {
                     n=vecteur[i];
@@ -13,6 +14,16 @@ public class VectorHelper {
             }
         }
     }
+
+    public void invers(int[] vecteur){
+        int n;
+        for(int i=0 ; i<vecteur.length/2 ; i++){
+            n=vecteur[i];
+            vecteur[i]=vecteur[vecteur.length-1-i];
+            vecteur[vecteur.length-1-i]=n;
+        }
+    }
+
 
 
 }
