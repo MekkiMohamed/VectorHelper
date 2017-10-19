203529dae9c60d55926e42e83f50f717b323479b
ritsurito21 <y.kardjadja@gmail.com>
2017-10-19 13:46:43 +0100

    just added the (trie) function

--- VectorHelper.java
+++ VectorHelper.java
@@ -1,3 +1,18 @@
 public class VectorHelper {
 
+    private int n;
+
+    void trie(int[] vecteur){
+        for(int i=0;i<vecteur.length;i++){
+            for(int j=i+1 ; j<vecteur.length ; i++){
+                if(vecteur[i]>vecteur[j]) {
+                    n=vecteur[i];
+                    vecteur[i]=vecteur[j];
+                    vecteur[j]=n;
+                }
+            }
+        }
+    }
+
+
 }
